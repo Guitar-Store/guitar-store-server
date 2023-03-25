7487fcf6475681a261be8a181f0c044a64bf7757
danyw <danielwiejak@gmail.com>
2023-03-25 12:13:35 +0000

    add get, post

--- Routes/products-route.js
+++ Routes/products-route.js
@@ -4,12 +4,28 @@ const Product = require("../Models/product-model");
 const router = express.Router();
 
 // GET all products
-router.get("/", () => {});
+router.get("/", (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.status(200).json(products);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
 
 // GET product deals
 router.get("/deals", () => {});
 
-
-
+// ADD product to basket
+router.post("/", async (req, res) => {
+  const { modelName, modelNumber, productCode, price, description, image, category, quantity, rating, reviewsNum, inStock, offer, idealFor, color, brand, material, type } = req.body;
+  try {
+    const product = await Product.create({modelName, modelNumber, productCode, price, description, image, category, quantity, rating, reviewsNum, inStock, offer, idealFor, color, brand, material, type})
+  res.status(200).json(product);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
